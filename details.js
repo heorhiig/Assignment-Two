0806c8cc8680d729e4d5318785665b2a08c778b8
Heorhii Churhuliia <gio4929189@gmail.com>
2025-05-05 15:26:02 +0100

    Update bike template and fix styling

--- details.js
+++ details.js
@@ -3,19 +3,19 @@ const details = [
     title: "Auto Unlock",
     description:
       "The app senses when you're nearby to unlock automatically. GPS tracking so you know where your bike is and can track it anytime.",
-    images: ["../images/image--6.png", "../images/image--8.png"],
+    images: ["./images/image--6.png", "./images/image--8.png"],
   },
   {
     title: "Range & Integrated lights",
     description:
       "The removable battery has up to 70km battery autonomy and weighs only 2.4 kg. Lights integrated into the frame give you always-on visibility day and night.",
-    images: ["../images/image--9.png", "../images/image--7.png"],
+    images: ["./images/image--9.png", "./images/image--7.png"],
   },
   {
     title: "Hydraulic disc brakes & Lightweight",
     description:
       "Brakes with total stopping power the second you make contact. The removable battery has up to 70km battery autonomy and weighs only 2.4 kg. Lights integrated into the frame give you always-on visibility day and night.",
-    images: ["../images/image--11.png", "../images/image--10.png"],
+    images: ["./images/image--11.png", "./images/image--10.png"],
   },
 ];
 
